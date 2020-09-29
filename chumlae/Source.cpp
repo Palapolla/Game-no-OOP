c18831c130aafd36fb440765e6e7a06b1a00faac
Warapob Keatkongsang <warapob2544@gmail.com>
2020-09-29 23:23:35 +0700

    wall3 and enemy2

--- chumlae/Source.cpp
+++ chumlae/Source.cpp
@@ -49,10 +49,10 @@ int main() {
 	int animationFrame = 0,skillFrame = 0;
 	bool spaceCheck = false;
 
-
 	//---------------------------------------level 1----------------------------------------------//
 
 	bool lv1 = true;
+	
 	//Wall1//
 
 	sf::Texture wall1_tx;
@@ -66,7 +66,27 @@ int main() {
 	sf::RectangleShape wall1(sf::Vector2f(600,150));
 	wall1.setPosition(0, 310);
 	wall1.setTexture(&wall1_tx);
+
+	//wall2//
+
+	sf::Texture wall2_tx;
+	wall2_tx.loadFromFile("wall03.png");
+	if (!wall2_tx.loadFromFile("wall03.jpg")) {
+		printf("Loading. . .\n");
+	}
+	else {
+		printf("Loading wall 03 done!\n");
+	}
+	sf::RectangleShape wall2(sf::Vector2f(400, 150));
+	wall2.setPosition(900, 310);
+	wall2.setTexture(&wall2_tx);
 	
+	//wall3//
+
+	sf::RectangleShape wall3(sf::Vector2f(100, 400));
+	wall3.setPosition(700, 500);
+	wall3.setFillColor(sf::Color(0, 0, 0));
+
 	//Enemy01//
 
 	sf::Texture enemy01_tx;
@@ -84,9 +104,28 @@ int main() {
 	int enemy01SizeY = enemy01TextureSize.y / 21;
 	enemy01.setTextureRect(sf::IntRect(enemy01SizeX * 2, enemy01SizeY * 2, enemy01SizeX, enemy01SizeY));
 	enemy01.setPosition(1000, 200);
-	int enemyPosX = 0 , enemyMaxX = 0;
-	float enemy01FrameX = 0, enemy01FrameY = 9;
+	int  enemyMaxX = 0;
+	float enemyPosX = 0, enemy01FrameX = 0, enemy01FrameY = 9;
 
+	//Enemy02//
+
+	sf::Texture enemy02_tx;
+	enemy02_tx.loadFromFile("Enemy2.png");
+	if (!enemy02_tx.loadFromFile("Enemy2.png")) {
+		printf("Loading. . .\n");
+	}
+	else {
+		printf("Loading Enemy02 Done!\n");
+	}
+	sf::RectangleShape enemy02(sf::Vector2f(100.0f, 100.0f));
+	enemy02.setTexture(&enemy02_tx);
+	sf::Vector2u enemy02TextureSize = enemy02_tx.getSize();
+	int enemy02SizeX = enemy02TextureSize.x / 13;
+	int enemy02SizeY = enemy02TextureSize.y / 21;
+	enemy02.setTextureRect(sf::IntRect(enemy02SizeX * 2, enemy02SizeY * 2, enemy02SizeX, enemy02SizeY));
+	enemy02.setPosition(900, 600);
+	int  enemy02MaxY = 0;
+	float enemy02PosY = 0, enemy02FrameX = 0, enemy02FrameY = 8;
 	
 	while (window.isOpen())
 	{
@@ -100,6 +139,7 @@ int main() {
 			player.setPosition(20, 600);
 			lv1 = false;
 		}
+
 		// ---------------------------------KeyboardInput------------------------------------// 
 
 		player.setTextureRect(sf::IntRect(playerSizeX * 0, playerSizeY * 2, playerSizeX, playerSizeY));
@@ -147,6 +187,7 @@ int main() {
 			}
 		}
 		//--------------------------------------enemy01 move------------------------------------------//
+
 		enemy01.move(enemyPosX, 0.0f);
 		enemy01.setTextureRect(sf::IntRect(enemy01SizeX * enemy01FrameX, enemy01SizeY * enemy01FrameY, enemy01SizeX, enemy01SizeY));
 		enemy01FrameX++;
@@ -154,7 +195,7 @@ int main() {
 			enemy01FrameX = 0;
 		}
 
-		//---Check Enemy 01 Position---//
+		//Check Enemy 01 Position//
 
 		if (enemyMaxX < 100) {
 			enemyPosX = -4.5;
@@ -171,13 +212,40 @@ int main() {
 			enemy01FrameY = 9;
 		}
 
+		//--------------------------------------enemy02 move------------------------------------------//
+
+		enemy02.move(0.0f, enemy02PosY);
+		enemy02.setTextureRect(sf::IntRect(enemy02SizeX * enemy02FrameX, enemy02SizeY * enemy02FrameY, enemy02SizeX, enemy02SizeY));
+		enemy02FrameX++;
+		if (enemy02FrameX >= 9) {
+			enemy02FrameX = 0;
+		}
+
+		//Check Enemy 02 Position//
+
+		if (enemy02MaxY < 50) {
+			enemy02PosY = -4.5;
+			enemy02MaxY++;
+		}
+		else if (enemy02MaxY >= 50 && enemy02MaxY < 100) {
+			enemy02FrameY = 10;
+			enemy02PosY = 4.5;
+			enemy02MaxY++;
+		}
+		else {
+			enemy02MaxY = 0;
+			enemy02PosY = 0;
+			enemy02FrameY = 8;
+		}
+
 		window.clear();
 		window.draw(background);
 		window.draw(wall1);
+		window.draw(wall2);
+		window.draw(wall3);
 		window.draw(enemy01);
+		window.draw(enemy02);
 		window.draw(player);
-		
-		//keyboardinput();
 		window.display();
 	}
 }
