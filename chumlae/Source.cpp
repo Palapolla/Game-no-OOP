9b6f759bd6c0196ddbfaf5cd6e7c19c5debf704b
Warapob Keatkongsang <warapob2544@gmail.com>
2020-09-29 15:16:30 +0700

    level 1 wall enemy01

--- chumlae/Source.cpp
+++ chumlae/Source.cpp
@@ -20,14 +20,14 @@ void delay(int number_of_seconds)
 int main() {
 	//window
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Game");
+	sf::RenderWindow window(sf::VideoMode(1240, 720), "Game");
 	printf("Opening window . . .\n");
 
 	//-------------------------------------background-------------------------------------------//
 
 	backgroundtexture.loadFromFile("Background.png");
 	sf::Sprite background(backgroundtexture);
-	background.setScale(0.4, 0.4);
+	background.setScale(0.6f, 0.5f);
 	
 	//--------------------------------------player & texture----------------------------------------//
 	
@@ -38,18 +38,56 @@ int main() {
 		printf("Loading. . .\n");
 	}
 	else {
-		printf("LoadFromFile Done!\n");
+		printf("Loading Player Done!\n");
 	}
-	sf::RectangleShape player(sf::Vector2f(80.0f, 80.0f));
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
 	player.setTexture(&playerTexture);
 	sf::Vector2u textureSize = playerTexture.getSize();
 	int playerSizeX = textureSize.x / 13;
 	int playerSizeY = textureSize.y / 21;
 	player.setTextureRect(sf::IntRect(playerSizeX* 2, playerSizeY * 2, playerSizeX, playerSizeY));
+	int animationFrame = 0,skillFrame = 0;
+	bool spaceCheck = false;
+
+
+	//---------------------------------------level 1----------------------------------------------//
+
+	bool lv1 = true;
+	//Wall1//
+
+	sf::Texture wall1_tx;
+	wall1_tx.loadFromFile("wall01.png");
+	if (!wall1_tx.loadFromFile("wall01.jpg")){
+		printf("Loading. . .\n");
+	}
+	else {
+		printf("Loading wall 01 done!\n");
+	}
+	sf::RectangleShape wall1(sf::Vector2f(600,150));
+	wall1.setPosition(0, 310);
+	wall1.setTexture(&wall1_tx);
 	
+	//Enemy01//
 
+	sf::Texture enemy01_tx;
+	enemy01_tx.loadFromFile("Enemy.png");
+	if (!enemy01_tx.loadFromFile("Enemy.png")) {
+		printf("Loading. . .\n");
+	}
+	else {
+		printf("Loading Enemy01 Done!\n");
+	}
+	sf::RectangleShape enemy01(sf::Vector2f(100.0f, 100.0f));
+	enemy01.setTexture(&enemy01_tx);
+	sf::Vector2u enemy01TextureSize = enemy01_tx.getSize();
+	int enemy01SizeX = enemy01TextureSize.x / 13;
+	int enemy01SizeY = enemy01TextureSize.y / 21;
+	enemy01.setTextureRect(sf::IntRect(enemy01SizeX * 2, enemy01SizeY * 2, enemy01SizeX, enemy01SizeY));
+	enemy01.setPosition(1000, 200);
+	int enemyPosX = 0 , enemyMaxX = 0;
+	float enemy01FrameX = 0, enemy01FrameY = 9;
 
-	int animationFrame = 0;
+	
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -58,7 +96,10 @@ int main() {
 				window.close();
 			}
 		}
-
+		if (lv1 == true) {
+			player.setPosition(20, 600);
+			lv1 = false;
+		}
 		// ---------------------------------KeyboardInput------------------------------------// 
 
 		player.setTextureRect(sf::IntRect(playerSizeX * 0, playerSizeY * 2, playerSizeX, playerSizeY));
@@ -66,7 +107,7 @@ int main() {
 			player.move(4.5f, 0.0f);
 			player.setTextureRect(sf::IntRect(playerSizeX * animationFrame, playerSizeY * 11, playerSizeX, playerSizeY));
 			printf("Keypress : D\n");
-			
+
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 			player.move(-4.5f, 0.0f);
@@ -83,15 +124,57 @@ int main() {
 			player.setTextureRect(sf::IntRect(playerSizeX * animationFrame, playerSizeY * 10, playerSizeX, playerSizeY));
 			printf("Keypress : S\n");
 		}
-		animationFrame++;
-		delay(45);
-		if (animationFrame >= 9) {
-			animationFrame = 0;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+			spaceCheck = true;
+			printf("Keypress : Space\n");
+		}
+		if (spaceCheck == true) {
+			skillFrame++;
+			player.setTextureRect(sf::IntRect(playerSizeX * skillFrame, playerSizeY * 2, playerSizeX, playerSizeY));
+			delay(100);
+			if (skillFrame >= 6) {
+				skillFrame = 0;
+				spaceCheck = false;
+				continue;
+			}
+		}
+		else {
+			animationFrame++;
+			delay(45);
+			if (animationFrame >= 9) {
+				animationFrame = 0;
+			}
+		}
+		//--------------------------------------enemy01 move------------------------------------------//
+		enemy01.move(enemyPosX, 0.0f);
+		enemy01.setTextureRect(sf::IntRect(enemy01SizeX * enemy01FrameX, enemy01SizeY * enemy01FrameY, enemy01SizeX, enemy01SizeY));
+		enemy01FrameX++;
+		if (enemy01FrameX >= 9) {
+			enemy01FrameX = 0;
 		}
 
+		//---Check Enemy 01 Position---//
+
+		if (enemyMaxX < 100) {
+			enemyPosX = -4.5;
+			enemyMaxX++;
+		}
+		else if (enemyMaxX >= 100 && enemyMaxX < 200) {
+			enemy01FrameY = 11;
+			enemyPosX = 4.5;
+			enemyMaxX++;
+		}
+		else {
+			enemyMaxX = 0;
+			enemyPosX = 0;
+			enemy01FrameY = 9;
+		}
 
 		window.clear();
 		window.draw(background);
+		window.draw(wall1);
+		window.draw(enemy01);
 		window.draw(player);
 		
 		//keyboardinput();
